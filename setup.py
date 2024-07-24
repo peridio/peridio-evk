d2e72e640335d948b3f2a63aaffc414750145c21
Justin Schneck <j.schneck@peridio.com>
2024-07-24 17:22:31 -0400

    0.1.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peridio_evk',
-    version='0.1.1',
+    version='0.1.2',
     author="Peridio Developers",
     author_email="support@peridio.com",
     description="The Peridio Evaluation Kit",
